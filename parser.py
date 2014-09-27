2eaa1cfd5b1781d1372d67557c04281fac548488
Cody Reichert <cody@reichertbrothers.com>
2014-09-26 22:00:04 -0500

    move some variables around

--- parser.py
+++ parser.py
@@ -7,12 +7,15 @@ from bs4 import Comment
 rawHtml = open('homepage.html').read()
 html = bs(rawHtml)
 
+# Just creating a new file manually for now
+result = 'result.html'
+
+# create final target file
+new = 'homepage.hamlet'
+
 # extract comments from html
 comments = html.findAll(text=lambda text:isinstance(text, Comment))
 [comment.extract() for comment in comments]
-
-# Just creating a new file manually for now
-result = 'result.html'
 
 # write the prettified html to a new file
 # This also puts all end tags on their own line
@@ -26,12 +29,10 @@ pattern = re.compile(r"\s*</.*>")
 # open prettified file for parsing
 newHtml = open(result).readlines()
 
-# create final target file
-new = 'homepage.hamlet'
-
 # write all lines except end tags to target file
 for line in newHtml:
   if pattern.match(line) is None:
+    print line
     with open(new, 'a') as newout:
       newout.write(line)
 
