31358e6b2be3c127e7494a4266c1f58e56ce98db
Cody Reichert <cody@reichertbrothers.com>
2014-09-26 21:20:02 -0500

    remove all comments from source file

--- parser.py
+++ parser.py
@@ -1,9 +1,14 @@
 import re
 from bs4 import BeautifulSoup as bs
+from bs4 import Comment
 
 # read the original html file, get a Soup object
 rawHtml = open('homepage.html').read()
 html = bs(rawHtml)
+
+# extract comments from html
+comments = html.findAll(text=lambda text:isinstance(text, Comment))
+[comment.extract() for comment in comments]
 
 # Just creating a new file manually for now
 result = 'result.html'
@@ -26,6 +31,5 @@ new = 'homepage.hamlet'
 # write all lines except end tags to target file
 for line in newHtml:
   if pattern.match(line) is None:
-    print line
     with open(new, 'a') as newout:
       newout.write(line)
