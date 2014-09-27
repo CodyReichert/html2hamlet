fdd71185ea70c78a3b4221882d3e1a76f81534d9
Cody Reichert <cody@reichertbrothers.com>
2014-09-26 21:07:05 -0500

    script is now outputting hamlet file

--- parser.py
+++ parser.py
@@ -1,13 +1,31 @@
 import re
-# from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup as bs
 
-rawHtml = open('homepage.html').readlines()
+# read the original html file, get a Soup object
+rawHtml = open('homepage.html').read()
 html = bs(rawHtml)
 
+# Just creating a new file manually for now
+result = 'result.html'
+
+# write the prettified html to a new file
+# This also puts all end tags on their own line
+with open(result, 'w') as resultout:
+    resultout.write(html.prettify('utf-8'))
+
+
+# regex pattern to find end tags
 pattern = re.compile(r"\s*</.*>")
 
-for line in rawHtml:
+# open prettified file for parsing
+newHtml = open(result).readlines()
+
+# create final target file
+new = 'homepage.hamlet'
+
+# write all lines except end tags to target file
+for line in newHtml:
   if pattern.match(line) is None:
     print line
-
-# print(html.prettify())
+    with open(new, 'a') as newout:
+      newout.write(line)
